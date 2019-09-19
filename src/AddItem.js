b4fe15fb56feba131b8e388a8e286bee24b40217
Steve <roberts.stephen.david@gmail.com>
2019-09-19 16:16:32 +0100

    added basic addItem functionality

--- src/AddItem.js
+++ src/AddItem.js
@@ -1,6 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Modal } from 'react-bootstrap';
 
 export const AddItem = () => {
 
-    return (<i className="fas fa-plus-circle"></i>)
+    const [displayModal, updateDisplayModal] = useState(false)
+
+    const handleAdd = () => {
+        updateDisplayModal(false)
+        console.log('added')
+    }
+
+    const handleClose = () => {
+        updateDisplayModal(false)
+        console.log('closed')
+    }
+
+    const renderModal = () => {
+        if(displayModal===true){
+        return (<Modal.Dialog>
+            <Modal.Header closeButton>
+                <Modal.Title>Add Item</Modal.Title>
+            </Modal.Header>
+
+            <Modal.Body>
+                <p>What item would you like to add?</p>
+                <input placeholder="Type in item here..."/>
+            </Modal.Body>
+
+            <Modal.Footer>
+                <Button variant="secondary" onClick = {()=>handleClose()}>Close</Button>
+                <Button variant="primary" onClick={()=>handleAdd()}>Add Item</Button>
+            </Modal.Footer>
+        </Modal.Dialog>)
+        } 
+    }
+
+    return (
+        <div>
+            {renderModal()}
+            <button onClick={()=>updateDisplayModal(true)}>
+                <i className="fas fa-plus-circle"></i>
+            </button>
+        </div>)
 }
